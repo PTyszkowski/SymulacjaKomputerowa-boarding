5b4dbc7304f6e9a09a0fc92b5cf21977b9a37138
PTyszkowski <piotr.tyszkowski99@gmail.com>
2020-12-07 16:49:53 +0100

    working model

--- Model.py
+++ Model.py
@@ -3,7 +3,9 @@ import random
 
 rows = 23
 columns = 6
-time = list(range(100000))
+time = []
+for i in range(1000000):
+    time.append([])
 
 def initialize():
     #seats dictionary = {seat_id(row_number, seat_bumber) : seat_status('0' - free, '1' - taken)}
@@ -21,7 +23,7 @@ def initialize():
     passangers = {}
     for i in range(rows * columns):
         seat = random.choice(seats_numeration)
-        passangers[i] = seat
+        passangers[i] = [seat,0]    #seat number, event_ongoing (deafault = 0)
         seats_numeration.remove(seat)
 
     aisle = []
@@ -46,22 +48,27 @@ def move_to_row():
         if passanger_id == -1:
             continue
 
-        if passangers[passanger_id][0] != aisle.index(passanger_id) and aisle[aisle.index(passanger_id) + 1] == -1:
+        if passangers[passanger_id][0][0] != aisle.index(passanger_id) and aisle[aisle.index(passanger_id) + 1] == -1:
             aisle [aisle.index(passanger_id) + 1] = passanger_id
             aisle[aisle.index(passanger_id)] = -1
 
 
 
-def start_putting_luggage():
-    t = np.random.normal(2, 1, 1)
+def start_putting_luggage(passanger_id): #1
+    t = -1
+    while t <= 0:
+        t = np.random.normal(2, 1, 1)
+        t = int(round(t[0]))
+    time[current_time + t].append((2,passanger_id))
+    passangers[passanger_id][1] = 2
     return t
 
-def stop_putting_luggage():
+def stop_putting_luggage(passanger_id):#2
+    start_moving_to_column(passanger_id)
 
-
-def start_move_to_column(passanger_id):
+def start_moving_to_column(passanger_id):#3
     t = 0
-    seat = passangers[passanger_id]
+    seat = passangers[passanger_id][0]
     if seat[1] == 2 or seat[1] == 3:
         t += 1
 
@@ -76,25 +83,37 @@ def start_move_to_column(passanger_id):
             t += 2
             if seats[seat[0],1] == 1:
                 t += 4
-    seats[seat] = 1
+    time[current_time + t].append((4,passanger_id))
+    passangers[passanger_id][1] = 4
     return t
+
+def stop_moving_to_column(passanger_id):#4
+    seat = passangers[passanger_id][0]
+    seats[seat] = 1
+    aisle[aisle.index(passanger_id)] = -1
+
+#main
 
 seats, passangers, aisle = initialize()
 queue = boarding_order('random')
-
 current_time = 0
+
 while sum(list(seats.values())) != columns*rows:
-    print(aisle)
     current_time += 1
     move_to_row()
-    if aisle[0] == -1 and queue != []:
-        enter_asile(queue.pop(0))
 
     for passanger_id in aisle:
         if passanger_id == -1:
             continue
-#zpisywanie zdarzeń do słownika i ich osługa
-        if passangers[passanger_id][0] == aisle.index(passanger_id):
-            start_putting_luggage()
-            move_to_column(passanger_id)
-            aisle[aisle.index(passanger_id)] = -1
+        if passangers[passanger_id][0][0] == aisle.index(passanger_id) and passangers[passanger_id][1] == 0:
+            start_putting_luggage(passanger_id)
+
+    for event in time[current_time]:
+        if event[0] == 2:
+            stop_putting_luggage(event[1])
+        if event[0] == 4:
+            stop_moving_to_column(event[1])
+
+    if aisle[0] == -1 and queue != []:
+        enter_asile(queue.pop(0))
+print(current_time)
